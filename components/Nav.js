893933c033b9a426b10aa12e01520d10cb11f6a2
bella <bella@bellalee.com>
2023-03-20 17:56:40 -0700

    Add new files

---
+++ components/Nav.js
@@ -0,0 +1,58 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import React, { useEffect, useState } from 'react';
+import Link from 'next/link';
+import styles from '@/styles/css/Nav.module.css';
+import cx from 'classnames';
+
+const links = [
+  {
+    page: "home",
+    route: "/"
+  },
+  {
+    page: "blog",
+    route: "../blog"
+  },
+];
+
+export default function Nav() {
+  const [isNavExpanded, setIsNavExpanded] = useState(false);
+
+  useEffect(() => {
+    if (isNavExpanded) document.body.classList.add('stop-scrolling');
+    else document.body.classList.remove('stop-scrolling');
+  }, [isNavExpanded])
+
+  const renderLinks = links.map((link) => {
+    const content = link.url ? <a href={link.url} target="_blank" rel="noreferrer">{link.page}</a> : <Link href={link.route}>{link.page}</Link>;
+
+    return (
+      <li
+        key={link.page}
+        className={styles.item}
+        onClick={() => setIsNavExpanded(false)}
+      >
+        {content}
+      </li>
+    )
+  });
+
+  return (
+    <nav className={styles.nav}>
+      <h1>
+        <Link href="/">Bella Lee</Link>
+      </h1>
+      <button
+        onClick={() => {
+          setIsNavExpanded(!isNavExpanded);
+        }}
+        className={styles.hamburger}>
+        <FontAwesomeIcon icon={faBars} size="2x" />
+      </button>
+      <ul className={cx(styles.menu, isNavExpanded ? null : styles.menu.hidden)}>
+        {renderLinks}
+      </ul>
+    </nav>
+  );
+};
