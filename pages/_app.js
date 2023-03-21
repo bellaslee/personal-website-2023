0136d0f4f6fac7b41a9ee950746ec893499c65ac
bella <bella@bellalee.com>
2023-03-20 17:54:30 -0700

    Complete initial setup
    Add blog main page, navigation bar, rough home page content. Figure
    out CSS modules and class naming conventions.

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
-import '@/styles/globals.css'
+import 'bootstrap/dist/css/bootstrap-grid.css'
+import '@/styles/css/styles.css'
+import Nav from '@/components/Nav';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Nav />
+      <Component {...pageProps} />
+    </>
+  )
 }
