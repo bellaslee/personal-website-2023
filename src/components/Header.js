191ae6bf79a8e00edb64715773cb81f81a88334e
bella <bella@bellalee.com>
2024-11-22 19:23:47 -0800

    Update header to link title to home page

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,19 +2,19 @@ import Link from "next/link";
 import React from "react";
 
 export default function Header({ font }) {
-  return (
-    <header className={font.className}>
-      <div className='title'>Bella Lee</div>
-      <nav>
-        <ul>
-          <li>
-            <Link href="/">About</Link>
-          </li>
-          <li>
-            <Link href="/blog">Blog</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  )
+    return (
+        <header className={font.className}>
+            <div className='title'><Link href="/">Bella Lee</Link></div>
+            <nav>
+                <ul>
+                    <li>
+                        <Link href="/">About</Link>
+                    </li>
+                    <li>
+                        <Link href="/blog">Blog</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
 }
